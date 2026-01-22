c186798045b5ea4c94d2d607976b3738e7e50c7c
Base21rizz <shoumikislamofficial@gmail.com>
2026-01-22 16:05:13 +0600

    Window settup

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+int main()
+{
+    // Window Dimensions
+    const int windowWidth{512};
+    const int windowHeight{380};
+
+    // Initialize Window
+    InitWindow(windowWidth, windowHeight, "Dapper-Dasher");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
+}
