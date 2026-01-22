f77743def9b0bd39f9d62452b9ad906beaf113ea
Base21rizz <shoumikislamofficial@gmail.com>
2026-01-22 16:18:19 +0600

    Jump

--- main.cpp
+++ main.cpp
@@ -8,11 +8,32 @@ int main()
     // Initialize Window
     InitWindow(windowWidth, windowHeight, "Dapper-Dasher");
 
+    // Rectangle Dimensions
+    const int width{50};
+    const int heigth{80};
+
+    int posY{windowHeight - heigth};
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        // Begin Drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        // Game Logic Start
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, heigth, BLUE);
+
+        // Game Logic End
+        // End Drawing
         EndDrawing();
     }
+    CloseWindow();
 }
